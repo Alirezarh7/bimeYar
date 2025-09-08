9e6cbef1790518863284a565e01c6261d79f50b7
Ali Ghasemi <ali.gha3mi75@mail.com>
2025-09-09 00:26:10 +0330

    personal-page create

---
+++ src/page/profile/personal-info/page.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "../../../component/page/profile/sidebar/Sidebar";
+
+
+const PersonalInfoPage = () => {
+    return (
+        <>
+            <div className="container">
+                <Sidebar />
+            </div>
+        </>
+    )
+}
+
+export default PersonalInfoPage;
--- src/router/paths.tsx
+++ src/router/paths.tsx
@@ -1,5 +1,6 @@
 import HomePage from "../page/page.tsx";
 import NotFoundPage from "../page/NotFound/page.tsx";
+import PersonalInfoPage from "../page/profile/personal-info/page.tsx";
 
 export interface RouteConfig {
   path: string;
@@ -10,5 +11,6 @@ export interface RouteConfig {
 
 export const Paths: RouteConfig[] = [
   { path: '*', label: 'صفحه مورد نظر یافت نشد', element: <NotFoundPage /> },
-  { path: '/', label: 'خانه', element: <HomePage />  },
+  { path: '/', label: 'خانه', element: <HomePage /> },
+  { path: '/profile/personal-info', label: 'اطلاعات شخصی', element: <PersonalInfoPage /> },
 ];
