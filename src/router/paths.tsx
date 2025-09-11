7232bcdb09091bda2be47d91948a9a04b3d4278d
Ali Ghasemi <ali.gha3mi75@mail.com>
2025-09-12 00:55:41 +0330

    update paths

--- src/router/paths.tsx
+++ src/router/paths.tsx
@@ -1,6 +1,7 @@
 import HomePage from "../page/page.tsx";
 import NotFoundPage from "../page/NotFound/page.tsx";
 import PersonalInfoPage from "../page/profile/personal-info/page.tsx";
+import InsurancePendingPage from "../page/profile/insurances/pending/page.tsx";
 
 export interface RouteConfig {
   path: string;
@@ -10,7 +11,16 @@ export interface RouteConfig {
 }
 
 export const Paths: RouteConfig[] = [
-  { path: '*', label: 'صفحه مورد نظر یافت نشد', element: <NotFoundPage /> },
-  { path: '/', label: 'خانه', element: <HomePage /> },
-  { path: '/profile/personal-info', label: 'اطلاعات شخصی', element: <PersonalInfoPage /> },
+  { path: "*", label: "صفحه مورد نظر یافت نشد", element: <NotFoundPage /> },
+  { path: "/", label: "خانه", element: <HomePage /> },
+  {
+    path: "/profile/personal-info",
+    label: "اطلاعات شخصی",
+    element: <PersonalInfoPage />,
+  },
+  {
+    path: "/profile/insurances/pending",
+    label: "در انتظار تمدید",
+    element: <InsurancePendingPage />,
+  },
 ];
