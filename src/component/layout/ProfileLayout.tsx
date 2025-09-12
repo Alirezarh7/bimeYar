166172bf50a3c946e76a411c16b386e715a1e229
Ali Ghasemi <ali.gha3mi75@mail.com>
2025-09-12 23:12:03 +0330

    profile layout

--- src/component/layout/ProfileLayout.tsx
+++ src/component/layout/ProfileLayout.tsx
@@ -9,12 +9,14 @@ type ProfileLayoutProps = {
 const ProfileLayout: React.FC<ProfileLayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-gray-50">
-      <aside className="hidden shrink-0 md:block">
+      <aside className="hidden shrink-0 md:block p-1 pb-1.5">
         <ProfileSidebar />
       </aside>
 
-      <div className="flex flex-col w-full">
-        <MobileHorizontalNav />
+      <div className="flex flex-col w-full p-2">
+        <div className="md:hidden">
+          <MobileHorizontalNav />
+        </div>
         <main className="flex-1 p-4 md:p-8">{children}</main>
       </div>
     </div>
