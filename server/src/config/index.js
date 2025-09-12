b70130ec42a2b6b2134734180f86f112b45af16c
alireza rahimi <alireza.ir76@gmail.com>
2025-09-12 10:31:02 +0330

    fixed bugs for deploy

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 module.exports = {
     port: process.env.PORT || 4000,
     origin: process.env.ORIGIN || 'http://localhost:5173',
-    jwtSecret: process.env.JWT_SECRET || 'dev-secret'
+    jwtSecret: process.env.JWT_SECRET || 'dev-secret',
+    baseUrl: process.env.BASE_URL || `http://localhost:${process.env.PORT || 4000}`
 };
