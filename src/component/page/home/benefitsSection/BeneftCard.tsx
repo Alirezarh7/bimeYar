f112bf18f5a9a9b64131c86d2eb3fa3e4fcc85c3
AmirHashemi77 <amirhashemi539@gmail.com>
2025-09-13 10:21:31 +0330

    fix some bugs

--- src/component/page/home/benefitsSection/BeneftCard.tsx
+++ src/component/page/home/benefitsSection/BeneftCard.tsx
@@ -8,7 +8,7 @@ const BeneftCard: FC<IProps> = ({ description, icon, title }) => {
   return (
     <div className="flex items-center flex-col justify-center bg-card p-5 w-full md:w-1/2 shadow-lg rounded-2xl gap-5">
       {icon}
-      <p className="text-sm text-primary font-semibold">{title}</p>
+      <p className="text-sm text-card-foreground font-semibold">{title}</p>
       <p className="text-sm text-gray-500 font-medium">{description}</p>
     </div>
   );
