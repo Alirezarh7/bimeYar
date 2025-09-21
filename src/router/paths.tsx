1fa0edc0112c581c0d62c241c2fff52552f432ca
Ali Ghasemi <ali.g4tt@gmail.com>
2025-09-21 18:56:26 +0330

    path changed

--- src/router/paths.tsx
+++ src/router/paths.tsx
@@ -27,19 +27,19 @@ export const Paths: RouteConfig[] = [
         element: <PersonalInfoPage/>,
     },
     {
-        path: "/profile/value",
+        path: "/profile/insurances/pending",
         label: "بیمه های در انتظار تمدید",
         element: <InsurancePendingPage/>,
     },
     {
         path: "/profile/insurances/incomplete",
         label: "بیمه های ناتمام",
-        element: <InsuranceIncompletePage/>,
+        element: <InsurancePurchasedPage/>,
     },
     {
         path: "/profile/insurances/purchased",
         label: "بیمه های خریداری شده",
-        element: <InsurancePurchasedPage/>,
+        element: <InsuranceIncompletePage />,
     },
     {
         path: "/profile/claims/vehicle-claims",
